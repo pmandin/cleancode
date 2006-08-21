06d880c724d063e298466021fb1c0a0e50bc4785
Patrice Mandin <patmandin@gmail.com>
2006-08-21 19:51:11 +0000

    Teamtap support

--- joystick/joypad.c
+++ joystick/joypad.c
@@ -33,7 +33,7 @@ struct JOYPAD_IO_S {
 	unsigned short dummy1[6];	/* 04,06,08,0a,0c,0e */
 	unsigned short paddles[4];	/* 10,12,14,16 */
 	unsigned short dummy2[4];	/* 18,1a,1c,1e */
-	unsigned short lightpens[2];/* 20,22 */ 
+	unsigned short lightpens[2];	/* 20,22 */ 
 } __attribute__((packed));
 #define JOYPAD_IO ((*(volatile struct JOYPAD_IO_S *)JOYPAD_IO_BASE))
 
@@ -54,16 +54,15 @@ enum {
 };
 
 /* Masks to read ports */
-enum {
-	PORT0_LINE0 = 0xfffe,
-	PORT0_LINE1 = 0xfffd,
-	PORT0_LINE2 = 0xfffb,
-	PORT0_LINE3 = 0xfff7,
-
-	PORT1_LINE0 = 0xffef,
-	PORT1_LINE1 = 0xffdf,
-	PORT1_LINE2 = 0xffbf,
-	PORT1_LINE3 = 0xff7f
+static const unsigned short joypad_masks[8*4]={
+	0xfffe, 0xfffd, 0xfffb, 0xfff7,
+	0xfff0, 0xfff1, 0xfff2, 0xfff3,
+	0xfff4, 0xfff5, 0xfff6, 0xfff8,
+	0xfff9, 0xfffa, 0xfffc, 0xffff,
+	0xffef, 0xffdf, 0xffbf, 0xff7f,
+	0xff0f, 0xff1f, 0xff2f, 0xff3f,
+	0xff4f, 0xff5f, 0xff6f, 0xff8f,
+	0xff9f, 0xffaf, 0xffcf, 0xffff
 };
 
 /*--- Defines ---*/
@@ -84,34 +83,25 @@ short jp_paddles[4];
 short jp_lightpens[2];
 unsigned short jp_directions;
 unsigned short jp_fires;
-unsigned long jp_joypads[2];
+unsigned long jp_joypads[8];
 
 short prev_jp_paddles[4];
 short prev_jp_lightpens[2];
 unsigned short prev_jp_directions;
 unsigned short prev_jp_fires;
-unsigned long prev_jp_joypads[2];
+unsigned long prev_jp_joypads[8];
 
-short prev_raw_lightpens[2], prev_raw_paddles[4], prev_raw_joypads[16];
-short raw_lightpens[2], raw_paddles[4], raw_joypads[16];
-char *raw_names[16]={
-	"port 0, fire, line 0 (pause, fire 0)",
-	"port 0, direction, line 0 (up, down, left, right)",
-	"port 0, fire, line 1 (-, fire 1)",
-	"port 0, direction, line 1 (*, 7, 4, 1)",
-	"port 0, fire, line 2 (-, fire 2)",
-	"port 0, direction, line 2 (0, 8, 5, 2)",
-	"port 0, fire, line 3 (-, option)",
-	"port 0, direction, line 3 (#, 9, 6, 3)",
-
-	"port 1, fire, line 0 (pause, fire 0)",
-	"port 1, direction, line 0 (up, down, left, right)",
-	"port 1, fire, line 1 (-, fire 1)",
-	"port 1, direction, line 1 (*, 7, 4, 1)",
-	"port 1, fire, line 2 (-, fire 2)",
-	"port 1, direction, line 2 (0, 8, 5, 2)",
-	"port 1, fire, line 3 (-, option)",
-	"port 1, direction, line 3 (#, 9, 6, 3)"
+long prev_raw_lightpens[2], prev_raw_paddles[4], prev_raw_joypads[8*8];
+long raw_lightpens[2], raw_paddles[4], raw_joypads[8*8];
+char *raw_names[8]={
+	"fire, line 0 (pause, fire 0)",
+	"direction, line 0 (up, down, left, right)",
+	"fire, line 1 (-, fire 1)",
+	"direction, line 1 (*, 7, 4, 1)",
+	"fire, line 2 (-, fire 2)",
+	"direction, line 2 (0, 8, 5, 2)",
+	"fire, line 3 (-, option)",
+	"direction, line 3 (#, 9, 6, 3)",
 };
 
 /*--- Functions prototypes ---*/
@@ -157,7 +147,7 @@ int main(int argc, char **argv)
 	for (i=0; i<4; i++) {
 		prev_raw_paddles[i] = raw_paddles[i] = 0;
 	}
-	for (i=0; i<16; i++) {
+	for (i=0; i<8*8; i++) {
 		prev_raw_joypads[i] = raw_joypads[i] = 0;
 	}
 
@@ -177,8 +167,8 @@ int main(int argc, char **argv)
 
 		Supexec(UpdateJoypads);
 
-/*		DisplayLogicalValues();*/
-		DisplayRawValues();
+		DisplayLogicalValues();
+/*		DisplayRawValues();*/
 	}
 }
 
@@ -240,8 +230,8 @@ void DisplayLogicalValues(void)
 		prev_jp_paddles[3] = jp_paddles[3];
 	}
 
-	/* Joypads (2 joypads, 2 axis, 17 buttons) */ 
-	for (i=0;i<2;i++) {
+	/* Joypads (8 joypads, 2 axis, 17 buttons) */ 
+	for (i=0;i<8;i++) {
 		if (jp_joypads[i] != prev_jp_joypads[i]) {
 			printf("Joypad %d: ",i);
 
@@ -258,58 +248,61 @@ void DisplayLogicalValues(void)
 				printf("right ");
 			}
 
-			if (jp_joypads[i] & (1<<JP_KPMULT)) {
-				printf("* ");
-			}
-			if (jp_joypads[i] & (1<<JP_KP7)) {
-				printf("7 ");
-			}
-			if (jp_joypads[i] & (1<<JP_KP4)) {
-				printf("4 ");
-			}
-			if (jp_joypads[i] & (1<<JP_KP1)) {
-				printf("1 ");
-			}
-
-			if (jp_joypads[i] & (1<<JP_KP0)) {
-				printf("0 ");
-			}
-			if (jp_joypads[i] & (1<<JP_KP8)) {
-				printf("8 ");
-			}
-			if (jp_joypads[i] & (1<<JP_KP5)) {
-				printf("5 ");
-			}
-			if (jp_joypads[i] & (1<<JP_KP2)) {
-				printf("2 ");
-			}
-
-			if (jp_joypads[i] & (1<<JP_KP9)) {
-				printf("9 ");
-			}
-			if (jp_joypads[i] & (1<<JP_KP6)) {
-				printf("6 ");
-			}
-			if (jp_joypads[i] & (1<<JP_KP3)) {
-				printf("3 ");
-			}
-
 			if (jp_joypads[i] & (1<<JP_PAUSE)) {
 				printf("pause ");
 			}
+			if (jp_joypads[i] & (1<<JP_OPTION)) {
+				printf("option ");
+			}
+
 			if (jp_joypads[i] & (1<<JP_FIRE0)) {
 				printf("a ");
 			}
 			if (jp_joypads[i] & (1<<JP_FIRE1)) {
 				printf("b ");
 			}
-
 			if (jp_joypads[i] & (1<<JP_FIRE2)) {
 				printf("c ");
 			}
-			if (jp_joypads[i] & (1<<JP_OPTION)) {
-				printf("option ");
+
+			if (jp_joypads[i] & (1<<JP_KPNUM)) {
+				printf("# ");
 			}
+			if (jp_joypads[i] & (1<<JP_KPMULT)) {
+				printf("* ");
+			}
+
+			if (jp_joypads[i] & (1<<JP_KP0)) {
+				printf("0 ");
+			}
+			if (jp_joypads[i] & (1<<JP_KP1)) {
+				printf("1 ");
+			}
+			if (jp_joypads[i] & (1<<JP_KP2)) {
+				printf("2 ");
+			}
+			if (jp_joypads[i] & (1<<JP_KP3)) {
+				printf("3 ");
+			}
+			if (jp_joypads[i] & (1<<JP_KP4)) {
+				printf("4 ");
+			}
+			if (jp_joypads[i] & (1<<JP_KP5)) {
+				printf("5 ");
+			}
+			if (jp_joypads[i] & (1<<JP_KP6)) {
+				printf("6 ");
+			}
+			if (jp_joypads[i] & (1<<JP_KP7)) {
+				printf("7 ");
+			}
+			if (jp_joypads[i] & (1<<JP_KP8)) {
+				printf("8 ");
+			}
+			if (jp_joypads[i] & (1<<JP_KP9)) {
+				printf("9 ");
+			}
+
 			printf("\n");
 
 			prev_jp_joypads[i] = jp_joypads[i];
@@ -336,9 +329,9 @@ void DisplayRawValues(void)
 		}
 	}
 
-	for (i=0; i<16; i++) {
+	for (i=0; i<8*8; i++) {
 		if (prev_raw_joypads[i] != raw_joypads[i]) {
-			printf("Joypad: %s=0x%04x\n", raw_names[i], raw_joypads[i]);
+			printf("Joypad %d: %s=0x%04x\n", (i>>3), raw_names[i&7], raw_joypads[i]);
 			prev_raw_joypads[i] = raw_joypads[i];
 		}
 	}
@@ -349,6 +342,7 @@ void DisplayRawValues(void)
 void UpdateJoypads(void)
 {
 	unsigned short tmp;
+	int i,j,cur_dir,cur_fire;
 
 	/* Update joysticks */
 	jp_fires = (~(JOYPAD_IO.fires)) & 15;
@@ -370,39 +364,33 @@ void UpdateJoypads(void)
 	tmp = raw_paddles[3] = JOYPAD_IO.paddles[3] & 255;
 	jp_paddles[3] = ((tmp<<8) | tmp) ^ 0x8000;
 
-	/* Update joypad 0 */	
-	JOYPAD_IO.directions=PORT0_LINE0;
-	jp_joypads[0]=((~(raw_joypads[0] = JOYPAD_IO.fires)) & 3)<<(16);
-	jp_joypads[0] |= ((~(raw_joypads[1] = JOYPAD_IO.directions))>>8) & 15;
+	/* Update teamtap 0 */	
+	for (i=0; i<4; i++) {
+		jp_joypads[i] = 0;
+		for (j=0; j<4; j++) {
+			JOYPAD_IO.directions = joypad_masks[(i*4)+j];
 
-	JOYPAD_IO.directions=PORT0_LINE1;
-	jp_joypads[0] |= ((~(raw_joypads[2] = JOYPAD_IO.fires)) & 3)<<(16+2);
-	jp_joypads[0] |= (((~(raw_joypads[3] = JOYPAD_IO.directions))>>8) & 15)<<4;
+			raw_joypads[(i<<3)+(j<<1)+0] = cur_fire = (~(JOYPAD_IO.fires) & 3)<<16;
+			raw_joypads[(i<<3)+(j<<1)+1] = cur_dir = (~(JOYPAD_IO.directions)>>8) & 15;
 
-	JOYPAD_IO.directions=PORT0_LINE2;
-	jp_joypads[0] |= ((~(raw_joypads[4] = JOYPAD_IO.fires)) & 3)<<(16+4);
-	jp_joypads[0] |= (((~(raw_joypads[5] = JOYPAD_IO.directions))>>8) & 15)<<8;
+			jp_joypads[i] |= cur_fire<<(j*2);
+			jp_joypads[i] |= cur_dir<<(j*4);
+		}
+	}
 
-	JOYPAD_IO.directions=PORT0_LINE3;
-	jp_joypads[0] |= ((~(raw_joypads[6] = JOYPAD_IO.fires)) & 3)<<(16+6);
-	jp_joypads[0] |= (((~(raw_joypads[7] = JOYPAD_IO.directions))>>8) & 15)<<12;
+	/* Update teamtap 1 */	
+	for (i=4; i<8; i++) {
+		jp_joypads[i] = 0;
+		for (j=0; j<4; j++) {
+			JOYPAD_IO.directions = joypad_masks[(i*4)+j];
 
-	/* Update joypad 1 */	
-	JOYPAD_IO.directions=PORT1_LINE0;
-	jp_joypads[1]=((~(raw_joypads[8] = JOYPAD_IO.fires)) & (3<<2))<<(16-2);
-	jp_joypads[1] |= ((~(raw_joypads[9] = JOYPAD_IO.directions))>>12) & 15;
+			raw_joypads[(i<<3)+(j<<1)+0] = cur_fire = (~(JOYPAD_IO.fires) & 0xc)<<14;
+			raw_joypads[(i<<3)+(j<<1)+1] = cur_dir = (~(JOYPAD_IO.directions)>>12) & 15;
 
-	JOYPAD_IO.directions=PORT1_LINE1;
-	jp_joypads[1] |= ((~(raw_joypads[10] = JOYPAD_IO.fires)) & (3<<2))<<(16);
-	jp_joypads[1] |= (((~(raw_joypads[11] = JOYPAD_IO.directions))>>12) & 15)<<4;
-
-	JOYPAD_IO.directions=PORT1_LINE2;
-	jp_joypads[1] |= ((~(raw_joypads[12] = JOYPAD_IO.fires)) & (3<<2))<<(16+2);
-	jp_joypads[1] |= (((~(raw_joypads[13] = JOYPAD_IO.directions))>>12) & 15)<<8;
-
-	JOYPAD_IO.directions=PORT1_LINE3;
-	jp_joypads[1] |= ((~(raw_joypads[14] = JOYPAD_IO.fires)) & (3<<2))<<(16+4);
-	jp_joypads[1] |= (((~(raw_joypads[15] = JOYPAD_IO.directions))>>12) & 15)<<12;
+			jp_joypads[i] |= cur_fire<<(j*2);
+			jp_joypads[i] |= cur_dir<<(j*4);
+		}
+	}
 
 	JOYPAD_IO.directions=0xffff;
 }
